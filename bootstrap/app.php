a362800ea39722d254235c1e782dcb1c18a7abad
farhanmuzakki <farhanmuzakki1501@gmail.com>
2026-07-28 01:34:20 +0700

    Fix trusted proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,11 +10,18 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function ($middleware) {
-    $middleware->alias([
-        'role' => \App\Http\Middleware\RoleMiddleware::class,
-    ]);
+    ->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->alias([
+            'role' => \App\Http\Middleware\RoleMiddleware::class,
+        ]);
+
+        $middleware->trustProxies(
+            at: '*'
+        );
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
